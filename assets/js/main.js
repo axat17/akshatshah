6b9f2ace0cbebe097a18c57d1254d8bc93d287ed
Akshat Shah <akshatshah@Akshats-MBP.home>
2017-12-25 22:33:38 -0500

    First Commit

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function ($) {
+    $('.level-bar-inner').css('width', '0');
+
+    $(window).on('load', function () {
+
+        $('.level-bar-inner').each(function () {
+
+            var itemWidth = $(this).data('level');
+
+            $(this).animate({
+                width: itemWidth
+            }, 800);
+
+        });
+    });
+    $('.level-label').tooltip();
+    $("#rss-feeds").rss(
+        {
+            limit: 3,
+            effect: 'slideFastSynced',
+            layoutTemplate: "<div class='item'>{entries}</div>",
+            entryTemplate: '<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>Read more</a></div>'
+
+        }
+    );
+
+    GitHubCalendar("#github-graph", "IonicaBizau");
+    GitHubActivity.feed({ username: "caseyscarborough", selector: "#ghfeed" });
+});
